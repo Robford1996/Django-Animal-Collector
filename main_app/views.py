f7564c2745fe6bd5aff01275875ad9c62a88e003
Robert Ford <robert.ford1996@gmail.com>
2022-10-17 17:56:21 -0400

    adds SQL data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Animal
 # Create your views here.
 
 
@@ -11,21 +12,22 @@ def about(request):
     return render(request, 'about.html')
 
 
-class Animal:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, type, description, age):
-        self.name = name
-        self.type = type
-        self.description = description
-        self.age = age
+# class Animal:  # Note that parens are optional if not inheriting from another class
+#     def __init__(self, name, type, description, age):
+#         self.name = name
+#         self.type = type
+#         self.description = description
+#         self.age = age
 
 
-animals = [
-    Animal('Rhino', 'Mammal', 'big horns', 40),
-    Animal('Turtle', 'reptile', 'hides in shell', 95),
-    Animal('bass', 'fish', 'eats', 5),
-    Animal('horse', 'mammal', 'runs', 25),
-]
+# animals = [
+#     Animal('Rhino', 'Mammal', 'big horns', 40),
+#     Animal('Turtle', 'reptile', 'hides in shell', 95),
+#     Animal('bass', 'fish', 'eats', 5),
+#     Animal('horse', 'mammal', 'runs', 25),
+# ]
 
 
 def animals_index(request):
+    animals = Animal.objects.all()
     return render(request, 'animals/index.html', {'animals': animals})
